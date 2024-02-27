b3f1b739b2fda4a0fe833fcf940cf7f08b481743
Sakar Aryal <techiesakar@gmail.com>
2024-02-27 18:27:33 +0545

    fix: api reponse fix

--- components/PageComponent/Courses/Curiculum/CourseCuriculum copy.jsx
+++ components/PageComponent/Courses/Curiculum/CourseCuriculum copy.jsx
@@ -1,17 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import axios from "../../../UI/Axios/Axios";
+import { useRouter } from "next/router";
 
 
-function CourseCuriculum({ data }) {
+function CourseCuriculum() {
   const [toggle, setToggle] = useState(false);
+  const [courseCuriculumData, setCourseCuriculumData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const [secID, setsecID] = useState(data);
+  const [secID, setsecID] = useState([]);
 
+  const router = useRouter();
+  useEffect(() => {
+    if (router.isReady) {
+      const { id } = router.query;
+      getCuriculumData(id);
+    }
+  }, [router.isReady, router.query]);
+
+  function getCuriculumData(id) {
+    setLoading(true)
+    try {
+      axios
+        .get(`/courses/${id}`)
+        .then((res) => {
+          console.log(res?.data?.result, 'syllabus');
+          setsecID(res.data[0]);
+          setCourseCuriculumData(res.data?.result?.syllabus);
+          setLoading(false)
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false)
+
+        });
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+
+    }
+  };
 
   return (
     <div>
       <div className=" bg-gray-50 p-3 overflow-y-auto rounded-md shadow-sm max-h-[800px]">
-        {data?.map((val, i) => {
+        <div className="flex flex-col gap-8">
+          {
+            loading && new Array(5).fill(0).map((_, i) => <div key={i} className="h-12 bg-gray-300 rounded-md w-full animate-pulse"></div>)
+          }
+        </div>
+        {!loading && courseCuriculumData?.map((val, i) => {
           return (
             <section key={val?.id}>
               <h2 className="my-0 text-sm font-medium text-gray-500">Chapter {i + 1}</h2>
